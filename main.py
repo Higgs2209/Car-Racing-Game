58e67264d8e5f6821d05b83d70baa8417dc99b3c
connor <connord79@gmail.com>
2024-01-06 21:26:53 +1100

    Car movement and basic game set up

---
+++ main.py
@@ -0,0 +1,118 @@
+import pygame
+import os
+import sys
+import time
+import math
+from utils import scale_image, blit_rotate_center
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+GRASS_PATH = resource_path("resources/grass.jpg")
+TRACK_PATH = resource_path("resources/track.png")
+TRACK_BORDER_PATH = resource_path("resources/track-border.png")
+FINISH_PATH = resource_path("resources/finish.png")
+RED_CAR_PATH = resource_path("resources/red-car.png")
+GREEN_CAR_PATH = resource_path("resources/green-car.png")
+
+GRASS = scale_image(pygame.image.load(GRASS_PATH), 2.5)
+TRACK = scale_image(pygame.image.load(TRACK_PATH), 0.9)
+TRACK_BORDER = scale_image(pygame.image.load(TRACK_BORDER_PATH), 0.9)
+FINISH = pygame.image.load(FINISH_PATH)
+RED_CAR = scale_image(pygame.image.load(RED_CAR_PATH), 0.55)
+GREEN_CAR = scale_image(pygame.image.load(GREEN_CAR_PATH), 0.55)
+
+WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Racing Game!")
+
+FPS = 60
+
+
+class AbstractCar:
+    def __init__(self, max_vel, rotation_vel):
+        self.img = self.IMG
+        self.max_vel = max_vel
+        self.vel = 0
+        self.rotation_vel = rotation_vel
+        self.angle = 0
+        self.x, self.y = self.START_POS
+        self.acceleration = 0.1
+
+
+    def rotate(self, left=False, right=False):
+        if left:
+            self.angle += self.rotation_vel
+        elif right:
+            self.angle -= self.rotation_vel
+
+    def draw(self, win):
+        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+
+    def move_forward(self):
+        self.vel = min(self.vel + self.acceleration, self.max_vel)
+        self.move()
+
+    def move(self):
+        radians = math.radians(self.angle)
+        vertical = math.cos(radians) * self.vel
+        horizontal = math.sin(radians) * self.vel
+
+        self.y -= vertical
+        self.x -= horizontal
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+
+class PlayerCar(AbstractCar):
+    IMG = RED_CAR
+    START_POS = (180, 200)
+
+
+def draw(win, images, player_car):
+    for img, pos in images:
+        win.blit(img, pos)
+    player_car.draw(win)
+    pygame.display.update()
+
+run = True
+clock = pygame.time.Clock()
+images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+player_car = PlayerCar(4, 4)
+
+while run:
+    clock.tick(FPS)
+
+    draw(WIN, images, player_car)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+    keys = pygame.key.get_pressed()
+    moved = False
+
+    if keys[pygame.K_a]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right=True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_forward()
+
+    if not moved:
+        player_car.reduce_speed()
+
+pygame.quit()
